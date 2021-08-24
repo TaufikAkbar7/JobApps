22a061474c82b1e9a93d98b1d8da651bacb73eea
TaufikAkbar7 <taufik.lol911@gmail.com>
2021-08-24 16:26:51 +0700

    fix

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const user = require('../routes/userRoutes');
-const tag = require('../routes/tagRoutes');
-
-router.use('/user', user);
-router.use('/tag', tag);
-
-module.exports = router;
